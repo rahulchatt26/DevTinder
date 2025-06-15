87d82fa7fdb29f9d59a3f51395a1144ae1ccb835
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2025-06-15 15:29:09 +0530

    various routes in express 5

--- src/app.js
+++ src/app.js
@@ -2,16 +2,47 @@ const express = require('express');
 
 const app = express();
 
-app.use('/',(req, res)=>{
-    res.send('Hello from Express!');
+
+app.get('/user',(req, res)=>{
+    res.send({firstName:'Rahul', lastName:'Chatterjee'});
 });
 
-app.use('/test',(req, res)=>{
+app.post('/user',(req, res) =>{
+    // inserting the user to DB
+    res.send('User created successfully');
+});
+
+app.put('/user',(req, res) =>{
+    // updating the user in DB
+    res.send('User updated successfully');
+});
+
+app.delete('/user',(req, res) =>{
+    // deleting the user from DB
+    res.send('User deleted successfully');
+});
+
+// Using route parameters
+app.post('/user/:userId/:passsword',(req, res) =>{
+    // inserting the user to DB
+    console.log(req.params)
+    res.send('User created successfully');
+});
+
+// Using query parameters
+app.post('/user',(req, res) =>{
+    // inserting the user to DB
+    console.log(req.query)
+    res.send('User created successfully');
+});
+
+// Using regular expressions in routes
+app.use(/.*ab$/,(req, res)=>{
     res.send('Hii there, how are you Mr. Test');
 });
 
-app.use('/hello',(req, res)=>{
-    res.send('Hello ji...!');
+app.get(/.*fly$/, (req, res) => {
+  res.send('/.*fly$/')
 });
 
 app.listen(3000,()=>{
